db09290bfe3d18514e3b012c8b8a3581fb187d53
klambert <kevleyski@gmail.com>
2019-04-13 09:00:37 +1000

    Secret Squirrel Brewery

---
+++ build/precache-manifest.43c79bb9203323961f81805ee48ee8ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a40d49f3d7ce89f4980",
+    "url": "/static/css/main.0a4e792c.chunk.css"
+  },
+  {
+    "revision": "2a40d49f3d7ce89f4980",
+    "url": "/static/js/main.cdedf968.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f4c49c398c163c3217e87e7542697536",
+    "url": "/index.html"
+  }
+];
