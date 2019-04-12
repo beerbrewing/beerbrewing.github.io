ee492a1f00f9558c0e4f07ee2e125c221d73e40b
klambert <kevleyski@gmail.com>
2019-04-13 09:32:27 +1000

    Secret Squirrel Brewery

---
+++ build/precache-manifest.d71b93c9ac08ca41b6ca55419379d0a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e962f1797f105e6a5a00",
+    "url": "/build/index.html/static/css/main.0a4e792c.chunk.css"
+  },
+  {
+    "revision": "e962f1797f105e6a5a00",
+    "url": "/build/index.html/static/js/main.52d6aff2.chunk.js"
+  },
+  {
+    "revision": "4a9ec3c3b8645a229bbc",
+    "url": "/build/index.html/static/js/runtime~main.ec96e156.js"
+  },
+  {
+    "revision": "4614de2c4e353bfe2b46",
+    "url": "/build/index.html/static/js/2.7e6d99fa.chunk.js"
+  },
+  {
+    "revision": "996bf1f307f8f84eac154d14b8b9212e",
+    "url": "/build/index.html/static/media/logo.996bf1f3.svg"
+  },
+  {
+    "revision": "eec1bc7e281146b286b479f30c0d1925",
+    "url": "/build/index.html/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f101907149e66734a96e04baa700f8c7.js"
+  "/build/index.html/precache-manifest.d71b93c9ac08ca41b6ca55419379d0a6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/build/index.html/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
