0eb9fca2d01ee34a26edf2fc935d8b212e7be176
Kevin Staunton-Lambert <kevleyski@users.noreply.github.com>
2019-09-18 03:22:21 +1000

    Pyrmont Brewery

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Secret Squirrel Brewery
+            Pyrmont Brewery<br>Sydney, Australia
           </p>
 
         </header>
