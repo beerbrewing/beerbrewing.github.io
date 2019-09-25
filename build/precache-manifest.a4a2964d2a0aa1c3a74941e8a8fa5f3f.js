72831f3c9764d6fbc2b5392665083a39a5c5a156
klambert <kevleyski@gmail.com>
2019-09-26 07:05:26 +1000

    Pyrmont Brewery

---
+++ build/precache-manifest.a4a2964d2a0aa1c3a74941e8a8fa5f3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "996bf1f307f8f84eac154d14b8b9212e",
+    "url": "/build/static/media/logo.996bf1f3.svg"
+  },
+  {
+    "revision": "4a3e9f4648ec8f9ba40ae1fa4f808e78",
+    "url": "/build/static/media/PyrmontBrewery_Kegerator.4a3e9f46.png"
+  },
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  },
+  {
+    "revision": "bad5d489aac2761546f1",
+    "url": "/build/static/js/main.582b55ba.chunk.js"
+  },
+  {
+    "revision": "81f87d994e9e6dce9308",
+    "url": "/build/static/js/2.8f4aac80.chunk.js"
+  },
+  {
+    "revision": "bad5d489aac2761546f1",
+    "url": "/build/static/css/main.af15b94e.chunk.css"
+  },
+  {
+    "revision": "961836cac8e8d71ff2027069ae82bccc",
+    "url": "/build/index.html"
+  }
+];
