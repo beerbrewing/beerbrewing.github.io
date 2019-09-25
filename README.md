7031f95c93d370c45d83e88aaa937639e636c67b
klambert <kevleyski@gmail.com>
2019-09-26 05:57:34 +1000

    Pyrmont Brewery

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## [Secret Squirrel Brewery Web Site](https://beerbrewing.github.io)
+## [Pyrmont Brewery Web Site](https://pyrmontbrewery.com)
 
-The Secret Squireel Brewery has been churning out the good stuff since 2007 in Pyrmont Sydney Australia
+The Pyrmont (Secret Squirrel) Brewery has been churning out the good stuff since 2007 in Pyrmont Sydney Australia
 
-We brew exclusively for [Switch Media](https://www.switch.tv/)
+Right now we brew exclusively for [Switch Media](https://www.switch.tv/)
 
 Being a fan of open source, the brewer @kevleyski is sharing some of his recipes in BeerXML format check out the [site](https://beerbrewing.github.io) for details
--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
+    let classLink = 'App-link';
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <br/><br/>
           <p>
-            Secret Squirrel Brewery
+            The Pyrmont (Secret Squirrel) Brewery has been churning out the good stuff since 2007 here in sunny Pyrmont, Sydney, Australia
+            <br/>
+            <br/>
+            We brew exclusively for local businesses to keep distribution cost minimal and stay well within our capacity
+            <br/>
+            Being a huge fan of open source and creative commons, the head brewer <a href={"https://twitter.com/kevleyski"} target={"_blank"} className={classLink}>
+            @kevleyski</a> is sharing a small selection of our beers:
           </p>
-
+          <ul>
+            <li><a href={"Beer Arrogant Bastard.xml"} target={"_blank"} className={classLink}>Beer Arrogant Bastard.xml</a> - American Strong Ale</li>
+            <li><a href={"Beer Brexit.bsmx"} target={"_blank"} className={classLink}>Beer Brexit.bsmx</a> - British Bitter</li>
+            <li><a href={"Beer Demo Gremlins.bsmx"} target={"_blank"} className={classLink}>Beer Demo Gremlins.bsmx</a> - Australian Pale Ale</li>
+            <li><a href={"Beer Kaibrau Kolsch.bsmx"} target={"_blank"} className={classLink}>Beer Kaibrau Kolsch.bsmx</a> - German Kölsch</li>
+            <li><a href={"Beer Oompa Loomper.bsmx"} target={"_blank"} className={classLink}>Beer Oompa Loomper.bsmx</a> - Chocolate Porter</li>
+            <li><a href={"Beer Rudolf Red.bsmx"} target={"_blank"} className={classLink}>Beer Rudolf Red.bsmx</a> - Red Ale</li>
+            <li><a href={"Beer Seong Cerveza.bsmx"} target={"_blank"} className={classLink}>Beer Seong Cerveza.bsmx</a> - Cerveza</li>
+            <li><a href={"Beer Suz Saison.bsmx"} target={"_blank"} className={classLink}>Beer Suz Saison.bsmx</a> - French Saison</li>
+            </ul>
         </header>
       </div>
     );
