63d4ede040c0770820715ca4291879a5d7d4d96e
klambert <kevleyski@gmail.com>
2019-10-01 20:37:48 +1000

    Pyrmont Brewery possums

---
+++ build/precache-manifest.c676f5d5d8162d70dfb66a9c57a4a486.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c553da5145b7719b7e1ed4127748c1a",
+    "url": "/build/static/media/possums_in_pyrmont.4c553da5.jpg"
+  },
+  {
+    "revision": "9d44e6e7839b710ce1c16f27cfa9504c",
+    "url": "/build/static/media/logo.9d44e6e7.svg"
+  },
+  {
+    "revision": "4a3e9f4648ec8f9ba40ae1fa4f808e78",
+    "url": "/build/static/media/PyrmontBrewery_Kegerator.4a3e9f46.png"
+  },
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  },
+  {
+    "revision": "89080261cc5100b56354",
+    "url": "/build/static/js/main.670b6e94.chunk.js"
+  },
+  {
+    "revision": "95480d8633c1dc73b408",
+    "url": "/build/static/js/2.3790332f.chunk.js"
+  },
+  {
+    "revision": "89080261cc5100b56354",
+    "url": "/build/static/css/main.19dcaddc.chunk.css"
+  },
+  {
+    "revision": "617aeea1570facd3386b7296a1d611cf",
+    "url": "/build/index.html"
+  }
+];
