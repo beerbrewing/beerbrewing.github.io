36ebcfb0f6a84a8212e842763482d3a09d9d14dc
klambert <kevleyski@gmail.com>
2019-10-03 09:29:00 +1000

    Updates

--- precache-manifest.482e1728afba684bbb21ae678436f0fb.js
+++ precache-manifest.482e1728afba684bbb21ae678436f0fb.js
@@ -8,10 +8,6 @@ self.__precacheManifest = [
     "url": "/build/static/media/logo.9d44e6e7.svg"
   },
   {
-    "revision": "255b79c5ad64ec5f6bc96b2e1ebd1ee9",
-    "url": "/build/static/media/cheers.255b79c5.jpg"
-  },
-  {
     "revision": "4a3e9f4648ec8f9ba40ae1fa4f808e78",
     "url": "/build/static/media/PyrmontBrewery_Kegerator.4a3e9f46.png"
   },
@@ -20,19 +16,19 @@ self.__precacheManifest = [
     "url": "/build/static/js/runtime~main.9df7376b.js"
   },
   {
-    "revision": "bd3993358af155a88927",
-    "url": "/build/static/js/main.0a66f06b.chunk.js"
+    "revision": "f57ae79f5be6e4c07960",
+    "url": "/build/static/js/main.0ed17814.chunk.js"
   },
   {
-    "revision": "0d47608a0fbafc5173b4",
-    "url": "/build/static/js/2.6ce76188.chunk.js"
+    "revision": "ee346e97b82dc7f4c071",
+    "url": "/build/static/js/2.4a8610fa.chunk.js"
   },
   {
-    "revision": "bd3993358af155a88927",
+    "revision": "f57ae79f5be6e4c07960",
     "url": "/build/static/css/main.b907dbfb.chunk.css"
   },
   {
-    "revision": "cbfcc76e68507464cb855b8a404ceef3",
+    "revision": "6decc5fff1022b646da8ec161b262256",
     "url": "/build/index.html"
   }
 ];
