2c96f0305b22a4efeadf27351a47a376b519a29b
klambert <kevleyski@gmail.com>
2019-10-03 23:11:33 +1000

    Updates

--- precache-manifest.28a3b3a8ddd872a53d5f185481d84b4e.js
+++ precache-manifest.28a3b3a8ddd872a53d5f185481d84b4e.js
@@ -8,8 +8,8 @@ self.__precacheManifest = [
     "url": "/build/static/media/logo.9d44e6e7.svg"
   },
   {
-    "revision": "234e5d42144b0422d33b6efe052baaca",
-    "url": "/build/static/media/cheers.234e5d42.jpg"
+    "revision": "ef4055f4ffe335ae0ed7eb712251440a",
+    "url": "/build/static/media/cheers.ef4055f4.png"
   },
   {
     "revision": "a32d4d07fcf94c914522145e9fd118e0",
@@ -20,19 +20,19 @@ self.__precacheManifest = [
     "url": "/build/static/js/runtime~main.9df7376b.js"
   },
   {
-    "revision": "3e7ca85af2aa66b78992",
-    "url": "/build/static/js/main.c25779c0.chunk.js"
+    "revision": "108af5337559b3534170",
+    "url": "/build/static/js/main.625e9e35.chunk.js"
   },
   {
     "revision": "0d47608a0fbafc5173b4",
     "url": "/build/static/js/2.6ce76188.chunk.js"
   },
   {
-    "revision": "3e7ca85af2aa66b78992",
-    "url": "/build/static/css/main.fb912f9f.chunk.css"
+    "revision": "108af5337559b3534170",
+    "url": "/build/static/css/main.a193456e.chunk.css"
   },
   {
-    "revision": "ec83816efb39cc95dd447786679ef994",
+    "revision": "ebb5b3c24f3930862d00867ad5285760",
     "url": "/build/index.html"
   }
 ];
