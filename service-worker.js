a0e0ea2e9e5c7c70b14374f36db01c375302c65d
klambert <kevleyski@gmail.com>
2019-10-03 22:30:19 +1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.e9205e23af2d6558578a395f6ab08f06.js"
+  "/build/precache-manifest.d53f8fd9c86e5d96a8ccba186bf14c44.js"
 );
 
 workbox.clientsClaim();
