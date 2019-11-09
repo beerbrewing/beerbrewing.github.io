38b3c5486f0ebe56dbb251f8883df04c7a7f8c90
klambert <kevleyski@gmail.com>
2019-11-09 12:00:59 +1100

    Pyrmont Brewery

---
+++ build/precache-manifest.e0e1ee970eeaadeb536a3078f211d41d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae6f8b77fa0ae29c7ef2c934b48a6f8f",
+    "url": "/build/static/media/pyrmont_quarry.ae6f8b77.jpg"
+  },
+  {
+    "revision": "9d44e6e7839b710ce1c16f27cfa9504c",
+    "url": "/build/static/media/logo.9d44e6e7.svg"
+  },
+  {
+    "revision": "6126e1439814c1c817da7e37c54eed59",
+    "url": "/build/static/media/cheers.6126e143.png"
+  },
+  {
+    "revision": "a32d4d07fcf94c914522145e9fd118e0",
+    "url": "/build/static/media/PyrmontBrewery_Kegerator.a32d4d07.png"
+  },
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  },
+  {
+    "revision": "c0f0687967178e0cf444",
+    "url": "/build/static/js/main.4e62ffd9.chunk.js"
+  },
+  {
+    "revision": "0d47608a0fbafc5173b4",
+    "url": "/build/static/js/2.6ce76188.chunk.js"
+  },
+  {
+    "revision": "c0f0687967178e0cf444",
+    "url": "/build/static/css/main.1810a09a.chunk.css"
+  },
+  {
+    "revision": "1ce9768f2ca6987486a8952454c73a73",
+    "url": "/build/index.html"
+  }
+];
