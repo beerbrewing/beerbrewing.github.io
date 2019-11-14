f5b1c8cb8e52e1de258f7c73ddefdeeb884c8b80
klambert <kevleyski@gmail.com>
2019-11-14 21:49:23 +1100

    Pyrmont Brewery - purchase via email enquiry

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.7527c289b09d3fefcc1e57def6f25bbf.js"
+  "/build/precache-manifest.d77e695627c8c03434e0cbc47946fe90.js"
 );
 
 workbox.clientsClaim();
