768f047c9e13e80c08bb8c1697833a707b18e669
klambert <kevleyski@gmail.com>
2019-11-14 20:33:16 +1100

    Updates

--- precache-manifest.7527c289b09d3fefcc1e57def6f25bbf.js
+++ precache-manifest.7527c289b09d3fefcc1e57def6f25bbf.js
@@ -4,24 +4,28 @@ self.__precacheManifest = [
     "url": "/build/static/media/PyrmontBrewery_Kegerator.a32d4d07.png"
   },
   {
-    "revision": "a3ff0c23167ff842a03c",
-    "url": "/build/static/css/main.553a8414.chunk.css"
+    "revision": "25fb3429639885e636d8",
+    "url": "/build/static/css/main.0a8efe39.chunk.css"
   },
   {
     "revision": "b4d8ef2cb7791815737b",
     "url": "/build/static/js/runtime~main.9df7376b.js"
   },
   {
-    "revision": "d65be7cf41fa7370ad70",
-    "url": "/build/static/js/2.4f11b4cf.chunk.js"
+    "revision": "bc256d397911a6a3ba49",
+    "url": "/build/static/js/2.4c46caea.chunk.js"
+  },
+  {
+    "revision": "ba003fe4ece60d15d98c6748e44a2d9a",
+    "url": "/build/static/media/beer.ba003fe4.png"
   },
   {
     "revision": "9d44e6e7839b710ce1c16f27cfa9504c",
     "url": "/build/static/media/logo.9d44e6e7.svg"
   },
   {
-    "revision": "a3ff0c23167ff842a03c",
-    "url": "/build/static/js/main.a114da4f.chunk.js"
+    "revision": "25fb3429639885e636d8",
+    "url": "/build/static/js/main.6df5b9bf.chunk.js"
   },
   {
     "revision": "6126e1439814c1c817da7e37c54eed59",
@@ -32,15 +36,15 @@ self.__precacheManifest = [
     "url": "/build/static/media/wheelie.1360df44.png"
   },
   {
-    "revision": "a0af8abc07b77e1be0956c2fb1a6dd89",
-    "url": "/build/static/media/pyrmont_sandstone_buildings.a0af8abc.jpeg"
-  },
-  {
     "revision": "ae6f8b77fa0ae29c7ef2c934b48a6f8f",
     "url": "/build/static/media/pyrmont_quarry.ae6f8b77.jpg"
   },
   {
-    "revision": "3238efe2950f654df40da06bdd4edfc4",
+    "revision": "a0af8abc07b77e1be0956c2fb1a6dd89",
+    "url": "/build/static/media/pyrmont_sandstone_buildings.a0af8abc.jpeg"
+  },
+  {
+    "revision": "2097914c4add5609f131d80e4b355c01",
     "url": "/build/index.html"
   }
 ];
