48aae12ca8c88b22ee79631ea2ea0553f124e8d3
klambert <kevleyski@gmail.com>
2019-11-25 20:31:28 +1100

    Pyrmont Brewery - License is held up

--- build/precache-manifest.1f10c9d820bd9100a882685327616bb2.js
+++ build/precache-manifest.1f10c9d820bd9100a882685327616bb2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "9d44e6e7839b710ce1c16f27cfa9504c",
-    "url": "/build/static/media/logo.9d44e6e7.svg"
+    "revision": "a32d4d07fcf94c914522145e9fd118e0",
+    "url": "/build/static/media/PyrmontBrewery_Kegerator.a32d4d07.png"
   },
   {
-    "revision": "528ccb8f36b69aeeb5b5",
+    "revision": "f49a6218c3875bc58925",
     "url": "/build/static/css/main.561e7ebb.chunk.css"
   },
   {
@@ -12,43 +12,43 @@ self.__precacheManifest = [
     "url": "/build/static/js/runtime~main.9df7376b.js"
   },
   {
-    "revision": "5ff92c3b9a9d3c4f5d39",
-    "url": "/build/static/js/2.d91caaa4.chunk.js"
+    "revision": "3afec3fbdfcf63526325",
+    "url": "/build/static/js/2.1c1a2d83.chunk.js"
   },
   {
     "revision": "ba003fe4ece60d15d98c6748e44a2d9a",
     "url": "/build/static/media/beer.ba003fe4.png"
   },
   {
-    "revision": "170b19fac8d50dc75d0514f1ab6f8bb8",
-    "url": "/build/static/media/buy_beer.170b19fa.png"
+    "revision": "9d44e6e7839b710ce1c16f27cfa9504c",
+    "url": "/build/static/media/logo.9d44e6e7.svg"
   },
   {
-    "revision": "528ccb8f36b69aeeb5b5",
-    "url": "/build/static/js/main.30a45168.chunk.js"
-  },
-  {
-    "revision": "a32d4d07fcf94c914522145e9fd118e0",
-    "url": "/build/static/media/PyrmontBrewery_Kegerator.a32d4d07.png"
+    "revision": "f49a6218c3875bc58925",
+    "url": "/build/static/js/main.b3462695.chunk.js"
   },
   {
     "revision": "6126e1439814c1c817da7e37c54eed59",
     "url": "/build/static/media/cheers.6126e143.png"
   },
   {
-    "revision": "1360df446deb5275903e365394adf20c",
-    "url": "/build/static/media/wheelie.1360df44.png"
-  },
-  {
     "revision": "ae6f8b77fa0ae29c7ef2c934b48a6f8f",
     "url": "/build/static/media/pyrmont_quarry.ae6f8b77.jpg"
+  },
+  {
+    "revision": "1360df446deb5275903e365394adf20c",
+    "url": "/build/static/media/wheelie.1360df44.png"
   },
   {
     "revision": "a0af8abc07b77e1be0956c2fb1a6dd89",
     "url": "/build/static/media/pyrmont_sandstone_buildings.a0af8abc.jpeg"
   },
   {
-    "revision": "e4c20c1b0b29507a7a5e33126f4cfd51",
+    "revision": "170b19fac8d50dc75d0514f1ab6f8bb8",
+    "url": "/build/static/media/buy_beer.170b19fa.png"
+  },
+  {
+    "revision": "0681e974597bd591cf370aaa205e99a6",
     "url": "/build/index.html"
   }
 ];
--- src/App.js
+++ src/App.js
@@ -72,66 +72,48 @@ class App extends Component {
             <p>
               Nestled amongst the <a href={"https://pyrmonthistory.net.au/industry/quarries"} target={"_blank"}
                              className={classLink}>Pyrmont yellowstone sandstone</a> hides a secret brewery!<br/>It’s been churning out the good stuff here in the city of Sydney, Australia since 2007
-              <br/>You can read about the <a href={"https://tinyurl.com/t2by8hz"} target={"_blank"} className={classLink}>history of brewing around Pyrmont and this little brewery here</a>
 
             <br/>
               <br/>
-              Are you living or working in Pyrmont? Want some local brews? You're in the right place!<br/>
-              <br/>
+              <b>The Pyrmont Brewery is <u>not</u> today in a position to sell beer, but<br />if you live or work in Pyrmont you are quite likely to bump in to it at various local meet-ups. <br />We'll let you know when that changes here and on social media<br/></b>
+              <br/>You can read about the <a href={"https://tinyurl.com/t2by8hz"} target={"_blank"} className={classLink}>history of brewing around Pyrmont and this little brewery here</a>
             </p>
             <span>
-            Beer from the Pyrmont Brewery purchasing options are:<br/><a href={"mailto:kegs@pyrmontbrewery.com?subject=Beer: Buy 50 litres&body=I'd like 50L Keg (A-type coupler) of ... please"} target={"_blank"}
-                                                         className={classLink}>50L DIN keg</a> (A-type coupler)<br/>
-            <a href={"mailto:kegs@pyrmontbrewery.com?subject=Beer: Buy 20 litres&body=I'd like 20L Keg (A-type coupler) of ... please"} target={"_blank"}
-               className={classLink}>20L tall keg</a> (A-type coupler)<br/>
-            <a href={"mailto:corny_kegs@pyrmontbrewery.com?subject=Beer: Buy 19 litres&body=I'd like 19L Corny Keg (ball lock) of ... please"} target={"_blank"}
-               className={classLink}>19L Cornelius keg</a> (ball lock) for your Kegerator<br/>
-            <a href={"mailto:cans@pyrmontbrewery.com?subject=Beer: Buy 330ml Cans&body=I'd like N x 330ml cans of ... please"} target={"_blank"}
-               className={classLink}>Cans 330ml</a> (100% recyclable)<br/>
-            </span>
-            Some of our heritage beers to choose from:<br/><br/>
+            Some of our heritage beers:<br/><br/>            </span>
+
 
             <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_halfway@pyrmontbrewery.com?subject=Beer: Half Way House&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 24"} target={"_blank"}
-                                                                 className={beerLink}><b>Half Way House</b><br/><i>Chocolate Stout</i><br/>4.5%<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="BeerDesc">Half Way House was the sandstone quarry and later a pub on Miller Street where the brewery is today. The pub didn't survive the Glebe Island bridge to Anzac bridge upgrade. <br/><br/>This beer is an English style double chocolate stout (and yes contains real Lindt couverture!)<br/><br/></div></div>
+                                                                 className={beerLink}><b>Half Way House</b><br/><i>Chocolate Stout</i><br/>4.5%<br/></a></div><div className="BeerDesc">Half Way House was the sandstone quarry and later a pub on Miller Street where the brewery is today. The pub didn't survive the Glebe Island bridge to Anzac bridge upgrade. <br/><br/>This beer is an English style double chocolate stout (and yes contains real Lindt couverture!)<br/><br/></div></div>
             <br/>
                <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_whingingpom@pyrmontbrewery.com?subject=Beer: Whinging Pom Bitter&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 24"} target={"_blank"}
-                                                                 className={beerLink}><b>Whinging Pom</b><br/><i>Real Ale</i><br/>4.4%<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="BeerDesc">British Bitter for my fellow expats of which there are many in Pyrmont! <br/><br/>A classic UK Maris Otter based Real Ale, with UK Fuggles and East Kent Goldings. Our English styles are lower in carbonation and if you want, can be gravity served straight from the keg without refrigeration<br/><br/></div></div>
+                                                                 className={beerLink}><b>Whinging Pom</b><br/><i>Real Ale</i><br/>4.4%<br/></a></div><div className="BeerDesc">British Bitter for my fellow expats of which there are many in Pyrmont! <br/><br/>A classic UK Maris Otter based Real Ale, with UK Fuggles and East Kent Goldings. Our English styles are lower in carbonation and if you want, can be gravity served straight from the keg without refrigeration<br/><br/></div></div>
             <br/>
                <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_maybanke@pyrmontbrewery.com?subject=Beer: Maybanke Dark Mild&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 24"} target={"_blank"}
-                                                                 className={beerLink}><b>Maybanke</b><br/><i>Dark Mild</i><br/>3.8%<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="BeerDesc">Maybanke Anderson set up and operated the first kindergarten school here in Australia. An amazing achievement! <br/><br/>This is an English Midlands dark mild similar to Banks'<br/><br/></div></div>
+                                                                 className={beerLink}><b>Maybanke</b><br/><i>Dark Mild</i><br/>3.8%<br/></a></div><div className="BeerDesc">Maybanke Anderson set up and operated the first kindergarten school here in Australia. An amazing achievement! <br/><br/>This is an English Midlands dark mild similar to Banks'<br/><br/></div></div>
             <br/>
                <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_arrogant@pyrmontbrewery.com?subject=Beer: Arrogan Harris Strong Ale&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 24"} target={"_blank"}
-                                                                 className={beerLink}><b>Arrogant Harris</b><br/><i>Strong Ale</i><br/>7.0%<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="BeerDesc">John Harris was surgeon to the New South Wales Corps, owned most of Ultimo and lived in Pyrmont. He didn't always get on with everyone and was court marshalled (twice)<br/><br/>This beer is inspired by the famously yummy brews 'you probably won't like' from San Diego, California<br/><br/></div></div>
+                                                                 className={beerLink}><b>Arrogant Harris</b><br/><i>Strong Ale</i><br/>7.0%<br/></a></div><div className="BeerDesc">John Harris was surgeon to the New South Wales Corps, owned most of Ultimo and lived in Pyrmont. He didn't always get on with everyone and was court marshalled (twice)<br/><br/>This beer is inspired by the famously yummy brews 'you probably won't like' from San Diego, California<br/><br/></div></div>
             <br/>
                <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_quaffer@pyrmontbrewery.com?subject=Beer: Quarry Quaffer Aussie Lager&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 24"} target={"_blank"}
-                                                                 className={beerLink}><b>Quarry Quaffer</b><br/><i>Aussie Lager</i><br/>4.9%<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="BeerDesc">"Like a wave of Angels crashing over me tongue!" [Dave 'Sensational' Robbo]<br/><br/>Very much like VB/Carlton it's sweet and bitter at the same time, Pride of Ringwood hops all the way, full strength lager beer<br/><br/></div></div>
+                                                                 className={beerLink}><b>Quarry Quaffer</b><br/><i>Aussie Lager</i><br/>4.9%<br/></a></div><div className="BeerDesc">"Like a wave of Angels crashing over me tongue!" [Dave 'Sensational' Robbo]<br/><br/>Very much like VB/Carlton it's sweet and bitter at the same time, Pride of Ringwood hops all the way, full strength lager beer<br/><br/></div></div>
             <br/>
                <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_cider@pyrmontbrewery.com?subject=Beer: Wokos Cloudy Apple Cider&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 24"} target={"_blank"}
-                                                                 className={beerLink}><b>Woko's</b><br/><i>Cloudy Cider</i><br/>5.1%<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="BeerDesc">James 'Woko' Watkinson was well known for teaching swimming and water polo in Pyrmont<br/><br/>This is a beautiful unfiltered apple cider made with English apples - it's slightly sweet but with a dry finish<br/><br/></div></div>
+                                                                 className={beerLink}><b>Woko's</b><br/><i>Cloudy Cider</i><br/>5.1%<br/></a></div><div className="BeerDesc">James 'Woko' Watkinson was well known for teaching swimming and water polo in Pyrmont<br/><br/>This is a beautiful unfiltered apple cider made with English apples - it's slightly sweet but with a dry finish<br/><br/></div></div>
             <br/>
                <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_paradise@pyrmontbrewery.com?subject=Beer: Paradise Saison&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 24"} target={"_blank"}
-                                                                 className={beerLink}><b>Paradise</b><br/><i>Saison</i><br/>6.8%<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="BeerDesc">Paradise yellowblock quarry is nearest the brewery, so named being the easiest sandstone to extract and work with, Sydney GPO building, Central, Art Gallery, RPA, Town Hall and many others (see below) all came from Paradise. <br/><br/>Stronger in alcohol but easy drinking traditional farmhouse style Saison, gently hopped with Centennial and Nelson Sauvignon<br/><br/></div></div>
+                                                                 className={beerLink}><b>Paradise</b><br/><i>Saison</i><br/>6.8%<br/></a></div><div className="BeerDesc">Paradise yellowblock quarry is nearest the brewery, so named being the easiest sandstone to extract and work with, Sydney GPO building, Central, Art Gallery, RPA, Town Hall and many others (see below) all came from Paradise. <br/><br/>Stronger in alcohol but easy drinking traditional farmhouse style Saison, gently hopped with Centennial and Nelson Sauvignon<br/><br/></div></div>
             <br/>
                <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_purgatory@pyrmontbrewery.com?subject=Beer: Purgatory Pale Ale&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 24"} target={"_blank"}
-                                                                 className={beerLink}><b>Purgatory</b><br/><i>Pale Ale</i><br/>4.9%<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="BeerDesc">Purgatory was the quarry round by the Glebe Island bridge. <br/><br/>This beer is similar to some Western Australian style 2-row pale ale with plenty of Challenger hops<br/><br/></div></div>
+                                                                 className={beerLink}><b>Purgatory</b><br/><i>Pale Ale</i><br/>4.9%<br/></a></div><div className="BeerDesc">Purgatory was the quarry round by the Glebe Island bridge. <br/><br/>This beer is similar to some Western Australian style 2-row pale ale with plenty of Challenger hops<br/><br/></div></div>
               <br/>
                <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_hellhole@pyrmontbrewery.com?subject=Beer: Hell Hole Helles&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 24"} target={"_blank"}
-                                                                 className={beerLink}><b>Hell Hole</b><br/><i>Helles</i><br/>4.7%<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="BeerDesc">Hell Hole was the quarry where the Sydney Fish Markets stands today, it used to flood a lot making it not much fun to deal with. <br/><br/>German Dortmunder style pilsner lager with Tassie Enigma hops<br/><br/></div></div>
+                                                                 className={beerLink}><b>Hell Hole</b><br/><i>Helles</i><br/>4.7%<br/></a></div><div className="BeerDesc">Hell Hole was the quarry where the Sydney Fish Markets stands today, it used to flood a lot making it not much fun to deal with. <br/><br/>German Dortmunder style pilsner lager with Tassie Enigma hops<br/><br/></div></div>
               <br/>
                <div className="Beer" style={{backgroundImage: `url(${quarryImage})`, opacity: 0.85}}><div className="BeerSnap"><a href={"mailto:kegs_mybrew@pyrmontbrewery.com?subject=Beer: My Own Beer&body=I'd like 20L Keg (A type) / 19L Corny keg (ball lock) / 330ml Cans X 60"} target={"_blank"}
-                    className={beerLinkYour}>Your Own<br/>Brew!<br/><img src={BuyThisBeer} alt="Buy This Beer" className={buybeer}/></a></div><div className="YourBeerDesc">Want something unique? If you can describe it, know a good example, maybe even have a recipe we'll have a go on the brewery Grainfather at recreating your brew with your name! (min 19L or 60 cans)<br/><br/></div></div>
+                    className={beerLinkYour}>Your Own<br/>Brew!<br/></a></div><div className="YourBeerDesc">Want something unique? If you can describe it, know a good example, maybe even have a recipe we'll have a go on the brewery Grainfather at recreating your brew with your name! (min 19L or 60 cans)<br/><br/></div></div>
             <br/>
-
             <span>
-                Will <i>only</i> deliver and pick-up our kegs within Pyrmont area
-                            <a href={"mailto:kegs@pyrmontbrewery.com?subject=Beer: Fill my Kegerator please&body=I'd like 50L Keg (A type) / 20L Keg (A type) / 19L Corny keg (ball lock) of ... please"} target={"_blank"}>
-
-                              <img src={PyrmontBrewery_Kegerator} width="70%" style={{opacity: 0.9}}></img></a>
-                <div>Borrow a <a href={"mailto:wheelie@pyrmontbrewery.com"} target={"_blank"}
-                    className={classLink}>mobile kegerator</a><br/>
-                    <small>(requires ice)</small>
-                    <img src={Wheelie} alt="" width="20%" style={{opacity: 0.9}}/>
-                </div>
+                <img src={PyrmontBrewery_Kegerator} width="70%" style={{opacity: 0.9}}></img>
                 <br/>
 
               </span>
@@ -169,20 +151,11 @@ class App extends Component {
               <img src={CheersThen} alt="" className={cheers}/>
             </p>
 
-            <p>
-              We brew beer exclusively for local businesses to keep the cost of distribution minimal and stay well
-              within our supply capacity <br/>(i.e. easily swing by with a keg and bring it back)<br/>
-              Beer usually takes a few weeks to ferment and condition so sometimes a little heads up is necessary<br/>
-              Pyrmont Brewery beer usually arrives chilled
-            </p>
-            <p>
-                We encourage <a href={"kevs_rsa.png"} target={"_blank"} className={classLink}>responsible</a> consumption of our brews, please take care of yourselves<br/>
-            </p>
             <br/>
               beers@pyrmontbrewery.com.au<br/>
               ABN: 47824483808<br/>
               Tel: 9692 0016
-            <br/><br/>
+              <br/><br/>
           </header>
       } </div>
   }
