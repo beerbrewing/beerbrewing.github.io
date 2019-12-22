ba6a9b90b4c08686cf6227ff2b61f408b90a6c6c
klambert <kevleyski@gmail.com>
2019-12-23 10:29:23 +1100

    Speccy

---
+++ build/precache-manifest.e21f387242080bae7cda03032f5553fe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "6126e1439814c1c817da7e37c54eed59",
+    "url": "/build/static/media/cheers.6126e143.png"
+  },
+  {
+    "revision": "2f4ae4a458745dc4c401",
+    "url": "/build/static/css/main.58fbe50e.chunk.css"
+  },
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  },
+  {
+    "revision": "3afec3fbdfcf63526325",
+    "url": "/build/static/js/2.1c1a2d83.chunk.js"
+  },
+  {
+    "revision": "ba003fe4ece60d15d98c6748e44a2d9a",
+    "url": "/build/static/media/beer.ba003fe4.png"
+  },
+  {
+    "revision": "9d44e6e7839b710ce1c16f27cfa9504c",
+    "url": "/build/static/media/logo.9d44e6e7.svg"
+  },
+  {
+    "revision": "a32d4d07fcf94c914522145e9fd118e0",
+    "url": "/build/static/media/PyrmontBrewery_Kegerator.a32d4d07.png"
+  },
+  {
+    "revision": "2f4ae4a458745dc4c401",
+    "url": "/build/static/js/main.05167ac8.chunk.js"
+  },
+  {
+    "revision": "ae6f8b77fa0ae29c7ef2c934b48a6f8f",
+    "url": "/build/static/media/pyrmont_quarry.ae6f8b77.jpg"
+  },
+  {
+    "revision": "1360df446deb5275903e365394adf20c",
+    "url": "/build/static/media/wheelie.1360df44.png"
+  },
+  {
+    "revision": "a0af8abc07b77e1be0956c2fb1a6dd89",
+    "url": "/build/static/media/pyrmont_sandstone_buildings.a0af8abc.jpeg"
+  },
+  {
+    "revision": "77b915fd404c1d633d4228fa366fa51e",
+    "url": "/build/static/media/pyrmont_brewery.77b915fd.woff2"
+  },
+  {
+    "revision": "170b19fac8d50dc75d0514f1ab6f8bb8",
+    "url": "/build/static/media/buy_beer.170b19fa.png"
+  },
+  {
+    "revision": "8a76cb3b1c19c1ec3004a5120288ac51",
+    "url": "/build/index.html"
+  }
+];
