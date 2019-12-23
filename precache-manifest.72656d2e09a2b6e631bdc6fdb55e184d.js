33de978de41c119de9008ffb14b0b92e3161435f
klambert <kevleyski@gmail.com>
2019-12-23 11:00:42 +1100

    Updates

--- precache-manifest.72656d2e09a2b6e631bdc6fdb55e184d.js
+++ precache-manifest.72656d2e09a2b6e631bdc6fdb55e184d.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/build/static/media/cheers.6126e143.png"
   },
   {
-    "revision": "2f4ae4a458745dc4c401",
-    "url": "/build/static/css/main.58fbe50e.chunk.css"
+    "revision": "112f701e710a7c0ba2f9",
+    "url": "/build/static/css/main.7a7b68a9.chunk.css"
   },
   {
     "revision": "b4d8ef2cb7791815737b",
@@ -28,8 +28,8 @@ self.__precacheManifest = [
     "url": "/build/static/media/PyrmontBrewery_Kegerator.a32d4d07.png"
   },
   {
-    "revision": "2f4ae4a458745dc4c401",
-    "url": "/build/static/js/main.05167ac8.chunk.js"
+    "revision": "112f701e710a7c0ba2f9",
+    "url": "/build/static/js/main.733830d1.chunk.js"
   },
   {
     "revision": "ae6f8b77fa0ae29c7ef2c934b48a6f8f",
@@ -52,7 +52,7 @@ self.__precacheManifest = [
     "url": "/build/static/media/buy_beer.170b19fa.png"
   },
   {
-    "revision": "8a76cb3b1c19c1ec3004a5120288ac51",
+    "revision": "9dae7ce8546fccea074739f6c7d055c4",
     "url": "/build/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.e21f387242080bae7cda03032f5553fe.js"
+  "/build/precache-manifest.72656d2e09a2b6e631bdc6fdb55e184d.js"
 );
 
 workbox.clientsClaim();
