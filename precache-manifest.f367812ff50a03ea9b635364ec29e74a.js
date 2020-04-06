fa47991da01617df0851a95579e834fac00ba295
klambert <kevleyski@gmail.com>
2020-04-06 11:00:04 +1000

    Updates

--- precache-manifest.f367812ff50a03ea9b635364ec29e74a.js
+++ precache-manifest.f367812ff50a03ea9b635364ec29e74a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "d62efb1196c8e6092560",
+    "revision": "82d0858df71859def51d",
     "url": "/build/static/css/main.ecd005ed.chunk.css"
   },
   {
-    "revision": "d62efb1196c8e6092560",
-    "url": "/build/static/js/main.34d16528.chunk.js"
+    "revision": "82d0858df71859def51d",
+    "url": "/build/static/js/main.c3afd02c.chunk.js"
   },
   {
     "revision": "b4d8ef2cb7791815737b",
@@ -96,7 +96,7 @@ self.__precacheManifest = [
     "url": "/build/static/media/label_generic.5d44d1f8.png"
   },
   {
-    "revision": "5cec3af5fdbaeb47672210564c8cbd43",
+    "revision": "ae18b467f406df01e9efb149924398a5",
     "url": "/build/index.html"
   }
 ];
