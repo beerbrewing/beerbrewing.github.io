120d28cd1ecb608afd83d230af8c82ffa8f6b4ea
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-06-23 08:17:54 +1000

    Adding Pyrmont Brewery Reference Library

---
+++ src/libraryData.js
@@ -0,0 +1,20 @@
+export const allArticles = [
+  {
+    id: 'brewing-basics',
+    title: 'Brewing Basics',
+    content: 'An introduction to the fundamental concepts of brewing beer.'
+  },
+  {
+    id: 'yeast-management',
+    title: 'Yeast Management',
+    content: 'Everything you need to know about handling and maintaining healthy yeast.'
+  }
+];
+
+export const categories = [
+  {
+    name: 'Getting Started',
+    articles: allArticles
+  }
+];
+
