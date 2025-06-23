1fc4314f1975acdaa81eb7943f69ffbf34929bd8
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-06-23 10:39:51 +1000

    Adding Pyrmont Brewery Reference Library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+// Create a root using the new createRoot API for React 18
+const root = createRoot(document.getElementById('root'));
+// Render the app using the root.render() method
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
