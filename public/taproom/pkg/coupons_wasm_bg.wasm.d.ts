b0c397c5ca66d3329f2be6f7acb18e1129603d86
Kevin Staunton-Lambert <music@pyrmontbrewery.com.au>
2025-08-15 15:21:14 +1000

    Pyrmont Brewery - Coupons

---
+++ public/taproom/pkg/coupons_wasm_bg.wasm.d.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const process_url: (a: number, b: number) => [number, number, number];
+export const process_url_sanitized: (a: number, b: number) => [number, number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
